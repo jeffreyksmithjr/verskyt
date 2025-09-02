34d078c6fc0a80128dc2d3f6cf4ee5f47326436f
jeffreyksmithjr <jeffreyksmithjr@gmail.com>
2025-09-01 23:40:43 -0400

    bump: version 0.2.4

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "Verskyt"
 copyright = "2025, Verskyt Contributors"
 author = "Verskyt Contributors"
-release = "0.2.3"
-version = "0.2.3"
+release = "0.2.4"
+version = "0.2.4"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
--- verskyt/__init__.py
+++ verskyt/__init__.py
@@ -4,7 +4,7 @@ Verskyt: A library for Tversky Neural Networks.
 Psychologically plausible deep learning with differentiable Tversky similarity.
 """
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 from verskyt.benchmarks import (
     XORBenchmark,
